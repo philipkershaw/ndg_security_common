206d839b98bc7044a531ad68ac8695e1984326be
Philip Kershaw <philipkershaw@users.noreply.github.com>
2011-05-24 15:04:28 +0000

    == 2.2.2 Release ==  * incorporates authentication relying party Paste template.  * candidate for new WPS and PyDAP installations.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ need to modify application code.
 
 setup(
     name =           		'ndg_security_common',
-    version =        		'2.2.1',
+    version =        		'2.2.2',
     description =           'NERC DataGrid Security package containing common '
                             'utilities used by both server and client '
                             'packages',
